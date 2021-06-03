58fb2de53935127d06002ccec9e821ab4c31100e
Blanca Hernandez <blamca1029@gmail.com>
2021-06-02 20:48:13 -0700

    refactored solution for roman numeral

--- roman-numeral.js
+++ roman-numeral.js
@@ -39,54 +39,51 @@ class RomanNumeral {
     this.number = integer;
   }
 
+  static CONVERSION = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+    20: 'XX',
+    30: 'XXX',
+    40: 'XL',
+    50: 'L',
+    60: 'LX',
+    70: 'LXX',
+    80: 'LXXX',
+    90: 'XC',
+    100: 'C',
+    200: 'CC',
+    300: 'CCC',
+    400: 'CD',
+    500: 'D',
+    600: 'DC',
+    700: 'DCC',
+    800: 'DCCC',
+    900: 'CM',
+    1000: 'M',
+    2000: 'MM',
+    3000: 'MMM',
+  }
+
   toRoman() {
     let str = this.number.toString();
     let result = [];
     let multiplier = '1';
     for (let idx = str.length -1; idx >= 0; idx -= 1) {
-      if (str[idx]!= 0) {
+      if (str[idx]!== 0) {
         let val = str[idx] * (Number(multiplier));
-        result.unshift(this._convert(val));
+        result.unshift(RomanNumeral.CONVERSION[val]);
       }
       multiplier += '0';
     }
     return result.join('');
-  }
-
-  _convert(value) {
-    let conversion = {
-      1: 'I',
-      2: 'II',
-      3: 'III',
-      4: 'IV',
-      5: 'V',
-      6: 'VI',
-      7: 'VII',
-      8: 'VIII',
-      9: 'IX',
-      10: 'X',
-      20: 'XX',
-      30: 'XXX',
-      40: 'XL',
-      50: 'L',
-      60: 'LX',
-      70: 'LXX',
-      80: 'LXXX',
-      90: 'XC',
-      100: 'C',
-      200: 'CC',
-      300: 'CCC',
-      400: 'CD',
-      500: 'D',
-      600: 'DC',
-      700: 'DCC',
-      800: 'DCCC',
-      900: 'CM',
-      1000: 'M',
-      2000: 'MM',
-      3000: 'MMM',
-    }
-    return conversion[value];
   }
 }
 
